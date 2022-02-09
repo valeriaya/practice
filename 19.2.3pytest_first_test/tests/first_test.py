568c503fcb5ac9968b43c031d4cf7f9497fe9b4b
valeriaya <91946076+valeriaya@users.noreply.github.com>
2022-02-09 15:29:15 +0300

    Create first_test.py

---
+++ 19.2.3pytest_first_test/tests/first_test.py
@@ -0,0 +1,19 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_calculator_correctly(self):
+        assert self.calc.division(self, 10, 2) == 5
+
+    def test_subtraction_calculator_correctly(self):
+        assert self.calc.subtraction(self, 8, 6) == 2
+
+    def test_adding_calculator_correctly(self):
+        assert self.calc.adding(self, 5, 5) == 10
